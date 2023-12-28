bcd55da8853fddfcc92d0a6864ee6f92f6b6a59e
thsWebstorm03 <webstorm03@gmail.com>
2023-12-28 17:36:00 +0800

    update webhook

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
+const cors = require("cors");
 
 const express = require("express");
-const axios = require( "axios");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -11,21 +11,20 @@ app.use(cors());
 // Create a telegram bot with its API KEY
 const trelloAPIKey = process.env.TRELLO_API_KEY;
 const trelloToken = process.env.TRELLO_API_TOKEN;
-const callbackURL = 'https://telegram-trello-webhook.onrender.com/';
+const callbackURL = "https://telegram-trello-webhook.onrender.com/";
 const boardID = process.env.TRELLO_BOARD_ID;
 
 async function createTrelloWebhook() {
-
    try {
-       const response = await axios.post('https://api.trello.com/1/webhooks', {
-           key: trelloAPIKey,
-           token: trelloToken,
-           callbackURL: callbackURL,
-           idModel: boardID
-       });
-       console.log('Webhook created successfully:', response.data);
+      const response = await axios.post("https://api.trello.com/1/webhooks", {
+         key: trelloAPIKey,
+         token: trelloToken,
+         callbackURL: callbackURL,
+         idModel: boardID,
+      });
+      console.log("Webhook created successfully:", response.data);
    } catch (error) {
-       console.error('Error creating webhook:', error);
+      console.error("Error creating webhook:", error);
    }
 }
 
@@ -35,58 +34,82 @@ async function createTrelloWebhook() {
 
 async function sendVideo(chatId, videoUrl, caption) {
    try {
-       const response = await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendVideo`, {
-           chat_id: chatId,
-           video: videoUrl,
-           caption: caption
-       });
-       console.log("Video sent successfully", response.data);
+      const response = await axios.post(
+         `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendVideo`,
+         {
+            chat_id: chatId,
+            video: videoUrl,
+            caption: caption,
+         }
+      );
+      console.log("Video sent successfully", response.data);
    } catch (error) {
-       console.error("Error sending video", error);
+      console.error("Error sending video", error);
    }
 }
 
 async function sendMessage(chatId, text) {
    try {
-       const response = await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
-           chat_id: chatId,
-           text: text,
-       });
-       console.log("Video sent successfully", response.data);
+      const response = await axios.post(
+         `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+         {
+            chat_id: chatId,
+            text: text,
+         }
+      );
+      console.log("Video sent successfully", response.data);
    } catch (error) {
-       console.error("Error sending video", error);
+      console.error("Error sending video", error);
    }
 }
 
 app.post("/", async (req, res) => {
    // Check if it's a card move event
-   console.log(req.body, 'req.body');
+   console.log(req.body, "req.body");
 
    if (
       req?.body?.action?.type === "updateCard" &&
       req?.body?.action?.data?.listAfter &&
       req?.body?.action?.data?.listAfter?.name == "Finished"
    ) {
-
       const cardTitle = req.body.action.data.card.name;
       const listname = req.body.action.data.listAfter.name;
       const cardId = req.body.action.data.card.id;
 
-      const videoURLs=[];
+      const videoURLs = [];
       try {
          // Fetch attachments from Trello
-         const attachmentsResponse = await axios.get(`https://api.trello.com/1/cards/${cardId}/attachments?key=${trelloAPIKey}&token=${trelloToken}`);
-         
-         if (attachmentsResponse.data) {
-            // console.log(attachmentsResponse, 'at');
-            attachmentsResponse.data.map((item, i) => sendMessage('6456284057', `Video ${i+1} - ${item.url}`));
-            // console.log(videoURLs, 'vi')
+         const attachmentsResponse = await axios.get(
+            `https://api.trello.com/1/cards/${cardId}/attachments?key=${trelloAPIKey}&token=${trelloToken}`
+         );
+         const cardResponse = await axios.get(
+            `https://api.trello.com/1/cards/${cardId}?key=${trelloAPIKey}&token=${trelloToken}`
+         );
+
+         if (cardResponse.data) {
+            const cardDescription = cardResponse.data.desc;
+            const jsonData = JSON.parse(cardDescription);
+            const user_chat_id =
+               Object.keys(jsonData).indexOf("chatId") > -1
+                  ? jsonData.chatId
+                  : "6456284057";
+
+            console.log(user_chat_id, 'user_chat_id');
+
+            if (attachmentsResponse.data) {
+               // console.log(attachmentsResponse, 'at');
+               attachmentsResponse.data.map((item, i) =>
+                  sendMessage(
+                     user_chat_id,
+                     `Video ${i + 1} - ${item.fileName} \n ${item.url}`
+                  )
+               );
+               // console.log(videoURLs, 'vi')
+            }
          }
-     } catch (error) {
-         console.error('Error processing Trello card:', error);
-     }
-
-
+      } catch (error) {
+         console.error("Error processing Trello card:", error);
+      }
 
       // Send message to Telegram
       // axios
